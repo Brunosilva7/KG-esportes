4768f9d752b5a99f4cf213d273caade60afb6a4e
Brunosilva7 <brunocarneirosilva7@gmail.com>
2022-12-15 01:13:51 -0300

    [Adding] - KG logo without background image

--- src/assets/svg/Logo.jsx
+++ src/assets/svg/Logo.jsx
@@ -1,15 +1,18 @@
 import * as React from "react";
-
-function SvgComponent(props) {
+import LogoKG from "../img/logoKG.png"
+function SvgComponent() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={27} height={40} viewBox="0 0 27 40" {...props}>
-      <g data-name="Group 101" transform="translate(-375 -21)">
-        <rect data-name="Rectangle 16" width={13} height={8} rx={4} transform="translate(375 53)" fill="#f40051" />
-        <rect data-name="Rectangle 11" width={20} height={8} rx={4} transform="translate(382 21)" fill="#f40051" />
-        <rect data-name="Rectangle 15" width={20} height={8} rx={4} transform="translate(382 37)" fill="#f40051" />
-        <rect data-name="Rectangle 12" width={8} height={8} rx={4} transform="translate(375 29)" fill="#f40051" />
-      </g>
-    </svg>
+  <>
+  <img style={{width: '65px', height: '60px'}}src={LogoKG} alt="logo"/>
+  </>
+    // <svg xmlns="http://www.w3.org/2000/svg" width={27} height={40} viewBox="0 0 27 40" {...props}>
+    //   <g data-name="Group 101" transform="translate(-375 -21)">
+    //     <rect data-name="Rectangle 16" width={13} height={8} rx={4} transform="translate(375 53)" fill="#f40051" />
+    //     <rect data-name="Rectangle 11" width={20} height={8} rx={4} transform="translate(382 21)" fill="#f40051" />
+    //     <rect data-name="Rectangle 15" width={20} height={8} rx={4} transform="translate(382 37)" fill="#f40051" />
+    //     <rect data-name="Rectangle 12" width={8} height={8} rx={4} transform="translate(375 29)" fill="#f40051" />
+    //   </g>
+    // </svg>
   );
 }
 
